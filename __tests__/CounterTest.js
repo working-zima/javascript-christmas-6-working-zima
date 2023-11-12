ad4374bda9913b15bccfdf711b83423e7e384345
working-zima <working.zima@gmail.com>
2023-11-12 12:58:20 +0900

    test(CounterTest): 테스트 코드 수정

--- __tests__/CounterTest.js
+++ __tests__/CounterTest.js
@@ -1,30 +1,33 @@
 import Counter from '../src/components/Counter';
 
 describe('Counter 기능 테스트', () => {
-  const order1 = [
-    ['티본스테이크', 1, 55000],
-    ['바비큐립', 1, 54000],
-    ['초코케이크', 2, 15000],
-    ['제로콜라', 1, 3000],
-  ];
-  const order2 = [
-    ['타파스', 1, 5500],
-    ['제로콜라', 1, 3000],
-  ];
+  const order1 = '타파스 - 1,제로콜라 - 1';
+  const order2 = '티본스테이크 - 1,바비큐립 - 1,초코케이크 - 2,제로콜라 - 1';
+  const cost1 = 8500;
+  const cost2 = 142000;
 
-  test('할인 전 총주문 금액', () => {
-    const counter = new Counter(order1);
+  test.each([
+    [order1, cost1],
+    [order2, cost2],
+  ])('할인 전 총주문 금액', (orders, expected) => {
+    const counter = new Counter(orders);
+    counter.verifyOrder();
 
-    expect(counter.getTotalAmountBeforeDiscount()).toBe(142000);
+    const totalAmount = counter.getTotalAmountBeforeDiscount();
+
+    expect(totalAmount).toBe(expected);
   });
 
   test.each([
-    [true, [order1]],
-    [false, [order2]],
-  ])('샴페인 증정 이벤트', (expected, [order]) => {
+    [order1, false],
+    [order2, true],
+  ])('샴페인 증정 이벤트', (order, expected) => {
     const counter = new Counter(order);
+    counter.verifyOrder();
     counter.getTotalAmountBeforeDiscount();
 
-    expect(counter.canReceiveChampagne()).toBe(expected);
+    const canReceiveChampagne = counter.canReceiveChampagne();
+
+    expect(canReceiveChampagne).toBe(expected);
   });
 });
