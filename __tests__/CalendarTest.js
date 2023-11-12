fa432e8ac47cddaf85b117729fc2d03f17a295a6
working-zima <working.zima@gmail.com>
2023-11-12 19:08:40 +0900

    test(CalendarTest): isSpecialDiscountDay메서드 테스트

--- __tests__/CalendarTest.js
+++ __tests__/CalendarTest.js
@@ -19,11 +19,27 @@ describe('Calendar 기능 테스트', () => {
     [1, 1000],
     [25, 3400],
     [31, 0],
-  ])('크리스마스 디데이 할인율', (date, expected) => {
+  ])('크리스마스 디데이 할인율 테스트', (date, expected) => {
     const calendar = new Calendar(date);
 
     const discountedAmount = calendar.calculateChristmasDiscount();
 
     expect(discountedAmount).toBe(expected);
+  });
+
+  test.each([
+    [1, false],
+    [3, true],
+    [10, true],
+    [17, true],
+    [24, true],
+    [25, true],
+    [31, true],
+  ])('특별 할인 테스트', (date, expected) => {
+    const calendar = new Calendar(date);
+
+    const isSpecialDay = calendar.isSpecialDiscountDay();
+
+    expect(isSpecialDay).toBe(expected);
   });
 });
