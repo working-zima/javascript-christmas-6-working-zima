b3dfd66e4282f6ad024ced14dd83a2ad8e0b26d4
working-zima <working.zima@gmail.com>
2023-11-14 13:43:54 +0900

    test(CalendarTest): Calendar 테스트 완료

--- __tests__/CalendarTest.js
+++ __tests__/CalendarTest.js
@@ -1,45 +1,115 @@
 import Calendar from '../src/components/Calendar.js';
+import { DISCOUNT_DAY } from '../src/constants/magicNumber.js';
 
-describe('Calendar 기능 테스트', () => {
-  test.each([
-    [1, true],
-    [2, true],
-    [3, false],
-    [4, false],
-    [5, false],
-    [6, false],
-    [7, false],
-  ])('주말 확인', (date, expected) => {
-    const calendar = new Calendar(date);
+describe('Calendar 클래스', () => {
+  describe('isWeekend 메서드', () => {
+    // given
+    const week = [
+      { date: 1, expected: true },
+      { date: 2, expected: true },
+      { date: 3, expected: false },
+      { date: 4, expected: false },
+      { date: 5, expected: false },
+      { date: 6, expected: false },
+      { date: 7, expected: false },
+    ];
+    test.each(week)(
+      '인자로 금요일, 토요일에 해당하는 날짜가 주어진 경우, true를 반환한다.',
+      ({ date, expected }) => {
+        // when
+        const calendar = new Calendar(date);
 
-    expect(calendar.isWeekend()).toBe(expected);
+        // then
+        expect(calendar.isWeekend()).toBe(expected);
+      },
+    );
   });
 
-  test.each([
-    [1, 1000],
-    [25, 3400],
-    [31, 0],
-  ])('크리스마스 디데이 할인율 테스트', (date, expected) => {
-    const calendar = new Calendar(date);
+  describe('isSpecialDiscountDay 메서드', () => {
+    // given
+    const specialDays = [
+      { date: 1, expected: false },
+      { date: 3, expected: true },
+      { date: 10, expected: true },
+      { date: 17, expected: true },
+      { date: 24, expected: true },
+      { date: 25, expected: true },
+      { date: 31, expected: true },
+    ];
 
-    const discountedAmount = calendar.calculateChristmasDiscount();
+    test.each(specialDays)(
+      '인자로 3, 10, 17, 24, 25, 31이 주어진 경우, true를 반환한다.',
+      ({ date, expected }) => {
+        // when
+        const calendar = new Calendar(date);
+        const isSpecialDay = calendar.isSpecialDiscountDay();
 
-    expect(discountedAmount).toBe(expected);
+        // then
+        expect(isSpecialDay).toBe(expected);
+      },
+    );
   });
 
-  test.each([
-    [1, false],
-    [3, true],
-    [10, true],
-    [17, true],
-    [24, true],
-    [25, true],
-    [31, true],
-  ])('특별 할인 테스트', (date, expected) => {
-    const calendar = new Calendar(date);
+  describe('isChristmasDiscountAvailable 메서드', () => {
+    // given
+    const dates = [
+      { date: 1, expected: true },
+      { date: 25, expected: true },
+      { date: 26, expected: false },
+      { date: 31, expected: false },
+    ];
 
-    const isSpecialDay = calendar.isSpecialDiscountDay();
+    // when
+    test.each(dates)(
+      '인자로 25이하의 숫자가 주어진 경우, true를 반환한다.',
+      ({ date, expected }) => {
+        // when
+        const calendar = new Calendar(date);
+        const christmasDiscountAvailable =
+          calendar.isChristmasDiscountAvailable();
 
-    expect(isSpecialDay).toBe(expected);
+        // then
+        expect(christmasDiscountAvailable).toBe(expected);
+      },
+    );
+  });
+
+  describe('getOrderDate 메서드', () => {
+    // given
+    const dates = [1, 2, 3, 4, 5, 6, 31];
+
+    test.each(dates)(
+      '인자로 특정 날짜가 주어진 경우, "12월 특정 날짜일"을 반환한다.',
+      date => {
+        // when
+        const calendar = new Calendar(date);
+        const orderDate = calendar.getOrderDate();
+
+        // then
+        expect(orderDate).toBe(`${DISCOUNT_DAY.MONTH + 1}월 ${date}일`);
+      },
+    );
+  });
+
+  describe('calculateChristmasDiscount 메서드', () => {
+    // given
+    const christmasDiscountAmount = [
+      { date: 1, expected: 1000 },
+      { date: 2, expected: 1100 },
+      { date: 25, expected: 3400 },
+      { date: 31, expected: 0 },
+    ];
+
+    test.each(christmasDiscountAmount)(
+      '인자로 1부터 25까지 가까워지는 숫자가 주어진 경우, 1,000원으로 시작하여 100원씩 증가된 숫자를 반환한다.',
+      ({ date, expected }) => {
+        // when
+        const calendar = new Calendar(date);
+        const discountedAmount = calendar.calculateChristmasDiscount();
+
+        // then
+        expect(discountedAmount).toBe(expected);
+      },
+    );
   });
 });
